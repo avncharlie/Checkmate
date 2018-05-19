6f29ab933f70de351a4ae5bce2dab0a05fb7f203
Alvin Joy Charles <alvinjoycharles@gmail.com>
2018-05-19 16:52:50 +1000

    Icon added.

--- Checkmate/Checkmate/Menu.Designer.vb
+++ Checkmate/Checkmate/Menu.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Menu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Menu))
+        Me.SuspendLayout()
+        '
+        'Menu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(580, 489)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Menu"
+        Me.Text = "Checkmate"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- Checkmate/Checkmate/My Project/Application.Designer.vb
+++ Checkmate/Checkmate/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Checkmate.Form1
+            Me.MainForm = Global.Checkmate.Menu
         End Sub
     End Class
 End Namespace
